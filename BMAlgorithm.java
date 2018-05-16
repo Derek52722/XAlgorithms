68b13f3025b598d19760e40ccfb6c74b9c437d4e
derek52722 <liuxing252>
2018-05-16 15:45:42 +0800

    移除package

--- BMAlgorithm.java
+++ BMAlgorithm.java
@@ -1,5 +1,3 @@
-package algorithm;
-
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
--- Heap.java
+++ Heap.java
@@ -1,5 +1,3 @@
-package algorithm;
-
 import java.lang.reflect.Array;
 import java.util.Comparator;
 
